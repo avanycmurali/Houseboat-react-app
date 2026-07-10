1ffc623d3d10614112b0a0a7ff9c87645c5a87f8
avanycmurali <avanycmuralidharan@gmail.com>
2026-07-10 21:39:58 +0530

    Search boat added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddBoat from './components/AddBoat'
+import SearchBoat from './components/SearchBoat'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
     <AddBoat/>
+    <SearchBoat/>
       
     </>
   )
